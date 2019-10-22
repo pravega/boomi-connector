2facc35d5b0b776b6c2b19971345b6181e5b8422
sabuz-262 <omar.faruk.buet@gmail.com>
2019-10-22 16:07:19 -0700

    More constants added.

--- src/main/java/com/boomi/connector/pravega/Constants.java
+++ src/main/java/com/boomi/connector/pravega/Constants.java
@@ -15,13 +15,15 @@ package com.boomi.connector.pravega;
  *
  */
 public class Constants {
-    protected static final String DEFAULT_SCOPE = "examples";
-    protected static final String DEFAULT_STREAM_NAME = "helloStream";
+    protected static final String DEFAULT_SCOPE = "examplesBoomi";
+    protected static final String DEFAULT_STREAM_NAME = "helloBoomiStream";
     protected static final String DEFAULT_CONTROLLER_URI = "tcp://127.0.0.1:9090";
     
-    protected static final String DEFAULT_ROUTING_KEY = "helloRoutingKey";
-    protected static final String DEFAULT_MESSAGE = "hello world";
-    protected static final boolean DEFAULT_IS_ROUTING_KEY_NEEDED = false;
+    protected static final String DEFAULT_ROUTING_KEY = "helloBoomiRoutingKey";
+    protected static final String DEFAULT_ROUTING_CONFIG_VALUE = "salary";
+    protected static final String DEFAULT_MESSAGE = "hello boomi world 19";
+    protected static final String DEFAULT_JSON_MESSAGE = "{\"name\":\"sonoo\",\"salary\":600000.0,\"age\":27}";
+    protected static final boolean DEFAULT_IS_ROUTING_KEY_NEEDED = true;
     
     protected static final String URI_PROPERTY = "uri";
     protected static final String SCOPE_PROPERTY = "scope";
