102f2734e1bf6bebccb97c9f7f854ce6a2688ce8
Stu Arnett <stu.arnett@emc.com>
2019-11-14 16:22:08 -0600

    - added maxReadTimePerExecution parameter (SDPT-144) - fixed connector descriptor format for routingKeyType - upgraded Pravega client to 0.6.0 GA - fixed bug where Query operator would stop execution at a checkpoint

--- src/main/java/com/boomi/connector/pravega/PravegaQueryOperation.java
+++ src/main/java/com/boomi/connector/pravega/PravegaQueryOperation.java
@@ -37,30 +37,45 @@ public class PravegaQueryOperation extends BaseQueryOperation implements AutoClo
     protected void executeQuery(QueryRequest request, OperationResponse response) {
         Logger logger = response.getLogger();
         FilterData input = request.getFilter();
+        long executionStartTime = System.currentTimeMillis();
+        long eventCounter = 0;
 
         try {
-            logger.info(String.format("Reading all the events from %s/%s", pravegaConfig.getScope(), pravegaConfig.getStream()));
+            logger.info(String.format("Reading events from %s/%s", pravegaConfig.getScope(), pravegaConfig.getStream()));
             EventRead<String> event = null;
+            long maxDuration = readerConfig.getMaxReadTimePerExecution() * 1000; // convert to ms
             do {
                 try {
                     event = reader.readNextEvent(readerConfig.getReadTimeout());
                     if (event.getEvent() != null) {
-                        logger.log(Level.INFO, String.format("Read event size: %d", event.getEvent().length()));
+                        eventCounter++;
+                        logger.log(Level.FINE, String.format("Read event size: %d", event.getEvent().length()));
                         response.addPartialResult(input, OperationStatus.SUCCESS, "OK",
                                 "OK", ResponseUtil.toPayload(new ByteArrayInputStream(event.getEvent().getBytes())));
                     }
                 } catch (ReinitializationRequiredException e) {
                     // There are certain circumstances where the reader needs to be reinitialized
                     // TODO: what needs to be done here?  if we re-initialize, how do we resume?
-                    logger.log(Level.WARNING, "caught ReinitializationRequiredException - Pravega client needs to be reinitialized", e);
+                    logger.log(Level.WARNING, "Caught ReinitializationRequiredException - Pravega client needs to be reinitialized", e);
                 }
-            } while (event != null && event.getEvent() != null);
+                // keep looping as long as:
+                // - an event was read OR we hit a checkpoint
+                //   AND
+                // - maximum execution time has been set (greater than 0) AND our execution time is under that
+            } while ((event.getEvent() != null || event.isCheckpoint())
+                    && (maxDuration > 0 && System.currentTimeMillis() - executionStartTime < maxDuration));
 
-            logger.log(Level.INFO, String.format("No more events from %s/%s", pravegaConfig.getScope(), pravegaConfig.getStream()));
+            if (event.getEvent() == null)
+                logger.log(Level.INFO, String.format("No more events from %s/%s: exiting", pravegaConfig.getScope(), pravegaConfig.getStream()));
+            else
+                logger.log(Level.INFO, String.format("Hit maximum read time (start: %d ms, now: %d ms, max: %d seconds): exiting",
+                        executionStartTime, System.currentTimeMillis(), readerConfig.getMaxReadTimePerExecution()));
         } catch (Exception e) {
             logger.log(Level.SEVERE, String.format("Error reading from %s/%s", pravegaConfig.getScope(), pravegaConfig.getStream()), e);
             ResponseUtil.addExceptionFailure(response, input, e);
         }
+
+        logger.log(Level.INFO, String.format("Read %d events in %d ms", eventCounter, System.currentTimeMillis() - executionStartTime));
 
         response.finishPartialResult(input);
     }
--- src/main/java/com/boomi/connector/pravega/ReaderConfig.java
+++ src/main/java/com/boomi/connector/pravega/ReaderConfig.java
@@ -6,18 +6,23 @@ import java.util.Map;
 import java.util.UUID;
 
 class ReaderConfig {
+    public static final long DEFAULT_READ_TIMEOUT = 2000; // ms
+    public static final long DEFAULT_MAX_READ_TIME = 30; // seconds
+
     static ReaderConfig fromContext(OperationContext context) {
         ReaderConfig readerConfig = new ReaderConfig();
         Map<String, Object> props = context.getOperationProperties();
         String readerGroup = (String) props.get(Constants.READER_GROUP_PROPERTY);
-        if (readerGroup == null) readerGroup = 'R' + UUID.randomUUID().toString().replaceAll("-", "");
+        if (readerGroup == null) readerGroup = "boomi-reader-" + UUID.randomUUID().toString();
         readerConfig.setReaderGroup(readerGroup);
-        readerConfig.setReadTimeout((long) props.get(Constants.READ_TIMEOUT_PROPERTY));
+        readerConfig.setReadTimeout((long) props.getOrDefault(Constants.READ_TIMEOUT_PROPERTY, DEFAULT_READ_TIMEOUT));
+        readerConfig.setMaxReadTimePerExecution((long) props.getOrDefault(Constants.MAX_READ_TIME_PER_EXECUTION_PROPERTY, DEFAULT_MAX_READ_TIME));
         return readerConfig;
     }
 
     private String readerGroup;
     private long readTimeout;
+    private long maxReadTimePerExecution;
 
     public String getReaderGroup() {
         return readerGroup;
@@ -33,5 +38,13 @@ class ReaderConfig {
 
     public void setReadTimeout(long readTimeout) {
         this.readTimeout = readTimeout;
+    }
+
+    public long getMaxReadTimePerExecution() {
+        return maxReadTimePerExecution;
+    }
+
+    public void setMaxReadTimePerExecution(long maxReadTimePerExecution) {
+        this.maxReadTimePerExecution = maxReadTimePerExecution;
     }
 }
