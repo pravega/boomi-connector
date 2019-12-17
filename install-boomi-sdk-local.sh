ffe81d5a02b9e8094b89f5e6f5f2b12e128ac615
Stu Arnett <stu.arnett@emc.com>
2019-12-17 13:22:08 -0600

    build changes required by Boomi SDK 2.0.1 release

--- install-boomi-sdk-local.sh
+++ install-boomi-sdk-local.sh
@@ -3,16 +3,21 @@
 # Run this in the Boomi SDK distribution directory
 ##########
 THIS_DIR="$(pwd)"
+COMMON_SDK_JAR=$(find "${THIS_DIR}" -regex '.*\/common-sdk-[0-9]\.[0-9]\.[0-9]\(-SNAPSHOT\)?\.jar$')
+COMMON_SDK_VERSION=$(echo "${COMMON_SDK_JAR}" | sed 's/^.*-\([0-9]\.[0-9]\..*\)\.jar/\1/' )
 SDK_API_JAR=$(find "${THIS_DIR}" -regex '.*\/connector-sdk-api-[0-9]\.[0-9]\.[0-9]\(-SNAPSHOT\)?\.jar$')
 CONNECTOR_VERSION=$(echo "${SDK_API_JAR}" | sed 's/^.*-\([0-9]\.[0-9]\..*\)\.jar/\1/' )
 SDK_UTIL_JAR="${THIS_DIR}/connector-sdk-util-${CONNECTOR_VERSION}.jar"
 SDK_TEST_UTIL_JAR="${THIS_DIR}/connector-sdk-test-util-${CONNECTOR_VERSION}.jar"
 
 echo "CONNECTOR_VERSION=${CONNECTOR_VERSION}"
+echo "COMMON_SDK_VERSION=${COMMON_SDK_VERSION}"
+echo "COMMON_SDK_JAR=${COMMON_SDK_JAR}"
 echo "SDK_API_JAR=${SDK_API_JAR}"
 echo "SDK_UTIL_JAR=${SDK_UTIL_JAR}"
 echo "SDK_TEST_UTIL_JAR=${SDK_TEST_UTIL_JAR}"
 
+mvn install:install-file -Dfile="${COMMON_SDK_JAR}" -DartifactId=common-sdk -Dpackaging=jar -Dversion="${COMMON_SDK_VERSION}" -DgroupId=com.boomi
 mvn install:install-file -Dfile="${SDK_API_JAR}" -DartifactId=connector-sdk-api -Dpackaging=jar -Dversion="${CONNECTOR_VERSION}" -DgroupId=com.boomi
 mvn install:install-file -Dfile="${SDK_UTIL_JAR}" -DartifactId=connector-sdk-util -Dpackaging=jar -Dversion="${CONNECTOR_VERSION}" -DgroupId=com.boomi
 mvn install:install-file -Dfile="${SDK_TEST_UTIL_JAR}" -DartifactId=connector-sdk-test-util -Dpackaging=jar -Dversion="${CONNECTOR_VERSION}" -DgroupId=com.boomi
