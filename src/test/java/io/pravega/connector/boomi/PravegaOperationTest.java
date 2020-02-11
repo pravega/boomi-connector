cbb279f5675831cf62b125bbe562a7ac9b8c625e
sabuz-262 <omar.faruk.buet@gmail.com>
2020-02-10 17:12:42 -0800

    Comment remove.

--- src/test/java/io/pravega/connector/boomi/PravegaOperationTest.java
+++ src/test/java/io/pravega/connector/boomi/PravegaOperationTest.java
@@ -307,7 +307,7 @@ public class PravegaOperationTest {
         }
     }
 
-    //@Test
+    @Test
     public void testMaxReadPerExecution() throws Exception {
         String stream = "connector-test-max-read-time";
         long maxReadTime = 4L; // seconds
