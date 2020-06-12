8a426abeb98ffddeeed4e0ae011d0cf29b773627
farukm <omar.faruk.buet@gmail.com>
2020-06-12 12:41:46 -0700

    Constant edited.

--- src/main/java/io/pravega/connector/boomi/Constants.java
+++ src/main/java/io/pravega/connector/boomi/Constants.java
@@ -24,8 +24,6 @@ public class Constants {
     static final String INTERVAL = "interval";
     static final String TIME_UNIT = "unit";
 
-    static final String INTERVAL = "interval";
-    static final String TIME_UNIT = "unit";
 
     static final String READER_GROUP_PROPERTY = "readerGroup";
     static final String READ_TIMEOUT_PROPERTY = "readTimeout";
